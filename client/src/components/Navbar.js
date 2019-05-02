20e63aaa5f9becfcfbc2aa1292ea89819b4c2b3b
Jacob Hughes <hughesrva@gmail.com>
2019-05-02 18:15:58 -0400

    adding navbar menu

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,40 +3,85 @@ import Axios from "axios";
 import { Link } from "react-router-dom";
 
 class Navbar extends Component {
+  state = {
+    showBurger: false
+  };
+
   handleLogoutClick = () => {
     Axios.get("/api/logout");
+  };
+
+  toggleBurger = () => {
+    this.state.showBurger === false
+      ? this.setState({
+          showBurger: true
+        })
+      : this.setState({
+          showBurger: false
+        });
+  };
+
+  closeBurger = () => {
+    this.setState({
+      showBurger: false
+    });
   };
 
   render() {
     return (
       <nav className="navbar">
-        <div className="navbar-start">
-          <Link to="/profile">
-            <button type="button" className="navbar-item button">
-              Profile
-            </button>
-          </Link>
-          <Link to="/search">
-            <button type="button" className="navbar-item button">
-              Search
-            </button>
-          </Link>
-          <Link to="/social">
-            <button type="button" className="navbar-item button">
-              Social
-            </button>
-          </Link>
-        </div>
-        <div className="navbar-end">
-          <a href="/">
-            <button
-              type="button"
-              className="navbar-item button"
-              onClick={this.handleLogoutClick}
-            >
-              Log Out
-            </button>
-          </a>
+        <a
+          role="button"
+          class={
+            this.state.showBurger === true
+              ? "navbar-burger burger is-active"
+              : "navbar-burger burger"
+          }
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+          onClick={this.toggleBurger}
+        >
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </a>
+        <div
+          className={
+            this.state.showBurger === true
+              ? "navbar-menu is-active"
+              : "navbar-menu"
+          }
+        >
+          {" "}
+          <div className="navbar-start">
+            <Link to="/profile">
+              <button type="button" className="navbar-item button">
+                Profile
+              </button>
+            </Link>
+            <Link to="/search">
+              <button type="button" className="navbar-item button">
+                Search
+              </button>
+            </Link>
+            <Link to="/social">
+              <button type="button" className="navbar-item button">
+                Social
+              </button>
+            </Link>
+          </div>
+          <div className="navbar-end">
+            <a href="/">
+              <button
+                type="button"
+                className="navbar-item button"
+                onClick={this.handleLogoutClick}
+              >
+                Log Out
+              </button>
+            </a>
+          </div>
         </div>
       </nav>
     );
